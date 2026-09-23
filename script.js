d0edead68c4a136e8b13c6d003832704ca8dc16b
BANKAI <hamide.keni@gmail.com>
2026-09-23 20:17:24 +0100

    Updates javascript for rock paper scissors

--- script.js
+++ script.js
@@ -1,8 +1,6 @@
-function getHumanChoice() {
-    let userInput = window.prompt("Choose rock, paper or scissors")
-    return userInput
-    }
-
+let computerScore = 0
+let humanScore = 0
+let counter = 0
 
 function getComputerChoice() {
     let rps = ["rock", "paper", "scissors"]
@@ -11,27 +9,50 @@ function getComputerChoice() {
     return rps[computer]
 }
 
-let computerScore = 0
-let humanScore = 0
-let counter = 0
-while (counter != 5) {
-    let humanChoice = getHumanChoice()
-    let computerChoice = getComputerChoice()
 
-    if (humanChoice == computerChoice) {
-        console.log("This round was a tie")
-    }       // for when human wins
-    else if ((humanChoice == "rock" && computerChoice == "scissors") ||
-            (humanChoice == "paper" && computerChoice == "rock") ||
-            (humanChoice == "scissors" && computerChoice == "paper")
-    ) {
-        console.log("Human wins this round!")
-        humanScore++
-        counter++
-    }
-    else {
-        console.log("Computer wins this round!")
-        computerScore++
-        counter++
-    }
+const rockButton = document.getElementById("rockButton")
+const paperButton = document.getElementById("paperButton")
+const scissorsButton = document.getElementById("scissorsButton")
+
+const displayResult = document.getElementById("results")
+
+rockButton.addEventListener("click", () => {
+    playRound("rock")
+})
+paperButton.addEventListener("click", () => {
+    playRound("paper")
+})
+scissorsButton.addEventListener("click", () => {
+    playRound("scissors")
+})
+
+function playRound(humanChoice) {
+        if (counter != 5) {
+            let computerChoice = getComputerChoice()
+            displayResult.textContent = `Computer has chosen: ${computerChoice} and user has chosen: ${humanChoice}`
+
+            if (humanChoice == computerChoice) {
+                displayResult.textContent = "This round was a tie!"
+            }       // for when human wins
+            else if ((humanChoice == "rock" && computerChoice == "scissors") || //why does it have to be like this --> (())
+                    (humanChoice == "paper" && computerChoice == "rock") ||
+                    (humanChoice == "scissors" && computerChoice == "paper")
+            ) {
+                displayResult.textContent = "Human wins this round!"
+                humanScore++
+                counter++
+            }
+            else {
+                displayResult.textContent = "Computer wins this round!"
+                computerScore++
+                counter++
+            }
+        }
+        else {
+            displayResult.textContent = `Computers score: ${computerScore}`
+            displayResult.textContent = `Users score: ${humanScore}`
+
+            displayResult.textContent = "Refresh game to play again!"
+            return
+        }
 }
